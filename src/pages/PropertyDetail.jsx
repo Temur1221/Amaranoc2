d7dc86334d5bc8caa68c9f736323b54b89a943c8
Temur1221 <tem67904@gmail.com>
2026-07-11 15:21:20 +0300

    update propertyDetail

--- src/pages/PropertyDetail.jsx
+++ src/pages/PropertyDetail.jsx
@@ -88,7 +88,7 @@ export default function PropertyDetail() {
 
           <div className="grid grid-cols-7 text-center gap-y-3.5 py-4 text-sm font-medium text-gray-700">
             <div className="text-gray-300">1</div><div className="text-gray-300">2</div><div className="text-gray-300">3</div><div className="text-gray-300">4</div><div>5</div><div>6</div><div>7</div>
-            <div>8</div><div>9</div><div>10</div><div>11</div><div>12</div><div className="bg-orange-50 font-bold text-orange-500 rounded-full w-8 h-8 flex items-center justify-center mx-auto">13</div><div>14</div>
+            <div>8</div><div>9</div><div>10</div><div>11</div><div>12</div><div>13</div><div>14</div>
             <div>15</div><div>16</div><div>17</div><div>18</div><div>19</div><div>20</div><div>21</div>
             <div>22</div><div>23</div><div>24</div><div>25</div><div>26</div><div>27</div><div>28</div>
             <div>29</div><div>30</div><div className="text-gray-300">1</div><div className="text-gray-300">2</div><div className="text-gray-300">3</div><div className="text-gray-300">4</div><div className="text-gray-300">5</div>
