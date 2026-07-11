195f713a2907cb832c15091995a529642817adc6
Temur1221 <tem67904@gmail.com>
2026-07-11 15:22:58 +0300

    update service

--- src/pages/Service.jsx
+++ src/pages/Service.jsx
@@ -70,7 +70,6 @@ export default function Service() {
       <Gorcoxutyunner />
       
       <div className="flex justify-center items-start px-4 w-full">
-        {/* Ավելացվեց mt-12 (Margin Top), որը քարտերը կիջեցնի ներքև */}
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 max-w-7xl pt-32 pb-12">
           {SpasarkumData.map((item) => (
             <SpasarkumCard 
